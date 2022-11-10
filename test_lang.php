5f8c3d18390cb9eb02b367d2b6d8948a7ab1e37c
SonicYouth <kekout3@gmail.com>
2022-11-09 19:45:44 -0500

    Update test_lang.php

--- test_lang.php
+++ test_lang.php
@@ -1,6 +1,5 @@
 <?php
-echo "<h2>PHP is Fun!</h2>";
-echo "Hello world!<br>";
-echo "I'm about to learn PHP!<br>";
-echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+$files1 = scandir($dir);
+
+print_r($files1);
 ?>
